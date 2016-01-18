ecfa4aaf93fbfc276ad7b36c96fe225c78e58ed9
Sascha Gros <gros.sascha@gmail.com>
2016-01-18 18:27:06 +0100

    Add example definition file

---
+++ contracts.d.ts
@@ -0,0 +1,43 @@
+declare enum Origin {
+  LOCAL, // From here
+  IMPORTED // From elsewhere
+}
+
+/**
+ * Thing that we sell.
+ */
+interface Product {
+  /**
+   * Uniquely defines the _product_
+   * This must be unique, then
+   */
+  name: string;
+
+  category: Category; // Classification
+
+  /** Used for indexing */
+  tags?: Tag[];
+
+  /** Where it comes from */
+  origin?: Origin;
+
+  /** Additional info */
+  props: { [k:string]: string; };
+}
+
+interface Ordered {
+  /** Position for sorting @type integer */
+  order?: number;
+}
+
+interface Category extends Ordered {
+  /** Uniquely identifies the category @pattern [A-Z][a-z][0-9]*/
+  name: string;
+
+  /** Classification level @minimum 1 @maximum 5 */
+  level: number;
+}
+
+interface Tag extends Ordered {
+  name: string;
+}
